87674c08b64e4a75b8051a59b73b69e692a87438
GiYoung Mun <56053651+munn129@users.noreply.github.com>
2023-06-14 12:50:22 +0900

    Update OdometryReader.py

--- car/ioniq/OdometryReader.py
+++ car/ioniq/OdometryReader.py
@@ -14,11 +14,16 @@ class OdometryReader:
     def read_odom(self):
         self.CAN_data = self.bus.recv(0)
 
-        if self.CAN_data.arbitration_id == 640:
+        if  (self.CAN_data != None) and (self.CAN_data.arbitration_id == 640):
             self.data = self.db.decode_message(self.CAN_data.arbitration_id, self.CAN_data.data)
             self.RL_vel = self.data['Gway_Wheel_Velocity_RL']
             self.RR_vel = self.data['Gway_Wheel_Velocity_RR']
             self.velocity = (self.RL_vel + self.RR_vel)/2
+
+        if self.CAN_data == None:
+            self.RL_vel = 0
+            self.RR_vel = 0
+            self.velocity = 0
     
     def get_vel(self) -> float:
         return self.velocity
