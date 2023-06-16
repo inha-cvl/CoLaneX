2fa9c9d358521536592355e291c6793423d2e4a9
GiYoung Mun <56053651+munn129@users.noreply.github.com>
2023-06-16 13:36:45 +0900

    add dbc files

--- car/integration_test/main.py
+++ car/integration_test/main.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     
     # if Hz adjustment is required, code must be added.
     # CAUTION: DO NOT USING rospy sleep.
+    # ioniq -> params.ioniq
+    # i30 -> parmas.ioniq
     params = Params()
     ros_data_publisher = RosDataPublisher(**params.ioniq)
 
