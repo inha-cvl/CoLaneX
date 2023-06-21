3063dad6e25273284ca338f7d0aa086c5bd5260e
GiYoung Mun <56053651+munn129@users.noreply.github.com>
2023-06-21 17:59:45 +0900

    refactoring codes

--- car/integration_test/main.py
+++ car/integration_test/main.py
@@ -1,15 +1,13 @@
 import rospy
-import RosDataPublisher, Params
+import RosDataPublisher
+from Params import ioniq, i30
 
 if __name__ == '__main__':
 
     
     # if Hz adjustment is required, code must be added.
     # CAUTION: DO NOT USING rospy sleep.
-    # ioniq -> params.ioniq
-    # i30 -> parmas.ioniq
-    params = Params()
-    ros_data_publisher = RosDataPublisher(**params.ioniq)
+    ros_data_publisher = RosDataPublisher(ioniq)
 
     try:
         while not rospy.is_shutdown():
